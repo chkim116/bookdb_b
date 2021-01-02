1d166efe008b0442f2a1fb74a6d9715fd887cb71
chkim116 <dudnqnfqlc@naver.com>
2021-01-03 04:14:10 +0900

    크롤러 적용

--- controller/searchController.js
+++ controller/searchController.js
@@ -1,5 +1,8 @@
 import Axios from "axios";
 import dotenv from "dotenv";
+import cheerio from "cheerio";
+import iconv from "iconv-lite";
+
 dotenv.config();
 
 export const searchNaver = async (req, res) => {
@@ -18,5 +21,105 @@ export const searchNaver = async (req, res) => {
     } catch (err) {
         console.log(err);
         res.status(400).json(err.message);
+    }
+};
+
+export const interviewSearch = async (req, res) => {
+    const url =
+        "http://news.kyobobook.co.kr/people/interview.ink?orderclick=QBJ";
+    let interview = [];
+    try {
+        const getInterview = await Axios.get(url, {
+            responseType: "arraybuffer",
+        }).then((res) => res.data);
+        const decoded = iconv.decode(getInterview, "EUC-KR");
+        const $ = cheerio.load(decoded);
+        const $body = $("ul.list_type_webzine").children("li");
+        $body.each(function (i) {
+            while (i < 6) {
+                interview[i] = {
+                    title: $(this).find("div.title a").text(),
+                    url: $(this)
+                        .find("div.title a")
+                        .attr("href")
+                        .split("(")[1]
+                        .split(",")[0],
+                    detail: $(this).find("div.preview a").text(),
+                    imageUrl: $(this).find("div.thumb img").attr("src"),
+                    id: i++,
+                };
+            }
+        });
+        res.status(200).json(interview);
+    } catch (err) {
+        console.log(err);
+        res.status(400);
+    }
+};
+
+export const steadySearch = async (req, res) => {
+    const steadyUrl = `http://www.kyobobook.co.kr/bestSellerNew/steadyseller.laf?orderClick=D0b`;
+    let list = [];
+    try {
+        const kyobo = await Axios.get(steadyUrl, {
+            responseType: "arraybuffer",
+        }).then((res) => res.data);
+        const decodeding = iconv.decode(kyobo, "EUC-KR");
+        const $ = cheerio.load(decodeding);
+        const $bodyList = $("ul.list_type01").children("li");
+
+        $bodyList.each(function (i) {
+            list[i] = {
+                title: $(this).find("div.title strong").text(),
+                url: $(this).find("div.cover a").attr("href"),
+                imageUrl: $(this).find("img").attr("src"),
+                imageAlt: $(this).find("img").attr("alt"),
+                summary: $(this).find("div.subtitle").text(),
+                auth: $(this)
+                    .find("div.author")
+                    .text()
+                    .split("|")[0]
+                    .replace("저자 더보기", ""),
+                id: i++,
+            };
+        });
+        res.status(200).json(list);
+    } catch (err) {
+        console.log(err);
+        res.status(400);
+    }
+};
+
+export const bestSearch = async (req, res) => {
+    const { id } = req.params;
+    const url = `http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf?range=1&kind=${id}&orderClick=DAA&mallGb=KOR&linkClass=A`;
+    let list = [];
+    try {
+        const kyobo = await Axios.get(url, {
+            responseType: "arraybuffer",
+        }).then((res) => res.data);
+        const decoded = iconv.decode(kyobo, "EUC-KR");
+        const $ = cheerio.load(decoded);
+        const $bodyList = $("ul.list_type01").children("li");
+
+        $bodyList.each(function (i) {
+            list[i] = {
+                title: $(this).find("div.title strong").text(),
+                url: $(this).find("div.cover a").attr("href"),
+                imageUrl: $(this).find("img").attr("src"),
+                imageAlt: $(this).find("img").attr("alt"),
+                summary: $(this).find("div.subtitle").text(),
+                auth: $(this)
+                    .find("div.author")
+                    .text()
+                    .split("|")[0]
+                    .replace("저자 더보기", ""),
+                id: i++,
+            };
+        });
+        res.status(200).json(list);
+    } catch (err) {
+        console.log(err);
+        res.status(400);
     }
 };
--- router/searchRouter.js
+++ router/searchRouter.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { searchNaver } from "../controller/searchController";
+import {
+    searchNaver,
+    interviewSearch,
+    steadySearch,
+    bestSearch,
+} from "../controller/searchController";
 
 const searchRouter = express.Router();
 
 searchRouter.post("/", searchNaver);
+
+searchRouter.get("/interview", interviewSearch);
+searchRouter.get("/steady", steadySearch);
+searchRouter.get("/best/:id", bestSearch);
 
 export default searchRouter;
